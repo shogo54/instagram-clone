26cd97f519e22512c249e6c08410d2116208f82b
Shogo Akiyama <sakiyama@knox.edu>
2021-06-21 14:20:57 -0700

    Fix import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from '../src/containers/Login';
+// import Login from '../src/containers/Login';
 import Home from '../src/containers/Home';
 import './App.css';
 
