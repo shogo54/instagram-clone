e218c848176da63e90a92ac68a5d51920ec1f939
Shogo Akiyama <sakiyama@knox.edu>
2021-06-26 00:09:05 -0700

    Remove unnecessary import from Home component

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,11 +1,4 @@
-import React, { useState } from 'react';
-import { Container, Typography } from '@material-ui/core';
-import { Formik, Form, FormikProps } from 'formik';
-import { Link, RouteComponentProps  } from 'react-router-dom';
-import TextField from '../../components/TextField';
-import Button from '../../components/Button';
-import { signupValidationSchema } from '../../data/validation';
-import { auth } from '../../data/firebase';
+import React from 'react';
 import Signup from '../Signup';
 
 const Home: React.FC = () => {
