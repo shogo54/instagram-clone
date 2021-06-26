05a67d23be1d2d45d54636ca45fe5fc8420f440a
Shogo Akiyama <sakiyama@knox.edu>
2021-06-26 00:24:02 -0700

    Add minmum length of 6 for password validation

--- src/data/validation.ts
+++ src/data/validation.ts
@@ -4,10 +4,10 @@ export const signupValidationSchema = Yup.object().shape({
   userId: Yup.string().email().required(),
   fullName: Yup.string().required(),
   userName: Yup.string().required(),
-  password: Yup.string().required(),
+  password: Yup.string().required().min(6),
 });
 
 export const loginValidationSchema = Yup.object().shape({
   userId: Yup.string().email().required(),
-  password: Yup.string().required(),
+  password: Yup.string().required().min(6),
 });
