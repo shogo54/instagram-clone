6269cb8a3a11579c6fa8e06862e7254952844bee
Shogo Akiyama <sakiyama@knox.edu>
2021-06-25 23:05:50 -0700

    Create a basic button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MuiButton, { ButtonProps } from '@material-ui/core/Button';
+// import './index.css';
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  return (
+    <div className="btn">
+      <MuiButton variant="contained" {...props}>
+        {children}
+      </MuiButton>
+    </div>
+  );
+};
+
+export default Button;
