42861c97e70269a516d0e4a634d64d7b53465146
Shogo Akiyama <sakiyama@knox.edu>
2021-06-26 00:02:52 -0700

    Create form-values in modules

---
+++ src/modules/form-values.ts
@@ -0,0 +1,12 @@
+
+export interface ILoginFormValues {
+  userId: string;
+  password: string;
+}
+
+export interface ISignupFormValues {
+  userId: string;
+  fullName: string;
+  userName: string;
+  password: string;
+}
