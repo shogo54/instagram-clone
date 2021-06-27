f14fb905bf3fc1b13f756c8d236d68d9ac782902
Shogo Akiyama <sakiyama@knox.edu>
2021-06-26 17:36:30 -0700

    Redirect to root when the route does not match with any path

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Home from '../src/containers/Home';
 import Login from '../src/containers/Login';
 import Signup from '../src/containers/Signup';
@@ -9,12 +9,16 @@ import { AuthContext } from './containers/Auth';
 const Routes: React.FC = () => {
   const { user } = useContext(AuthContext);
   return user ? (
-    <Profile />
+    <Router>
+      <Route exact path='/' component={Profile} />
+      <Redirect to='/' />
+    </Router>
   ) : (
     <Router>
       <Route exact path='/' component={Home} />
       <Route exact path='/accounts/signup' component={Signup} />
       <Route exact path='/accounts/login' component={Login} />
+      <Redirect to='/' />
     </Router>
   );
 };
