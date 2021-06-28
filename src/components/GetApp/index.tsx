5f0ed079a4092d6d5618d072ea755166ec84b933
Shogo Akiyama <sakiyama@knox.edu>
2021-06-27 20:32:55 -0700

    Create GetApp component

---
+++ src/components/GetApp/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import appleStore from '../../assets/apple-store-download.png';
+import googlePlay from '../../assets/google-play-download.png';
+import { useStyles } from './style';
+
+const GetApp: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container direction='column' justify='center' alignItems='center'>
+      <Typography className={classes.text}>Get the app.</Typography>
+      <Grid
+        container
+        direction='row'
+        justify='center'
+        alignItems='center'
+        className={classes.box}
+      >
+        <Grid item>
+          <a
+            href='https://apps.apple.com/app/instagram/id389801252?vt=lo'
+            rel='noreferrer'
+            target='_blank'
+            className={classes.link}
+          >
+            <img
+              src={appleStore}
+              alt='Available on the App Store'
+              className={classes.img}
+            />
+          </a>
+        </Grid>
+        <Grid item>
+          <a
+            href='https://play.google.com/store/apps/details?id=com.instagram.android'
+            rel='noreferrer'
+            target='_blank'
+            className={classes.link}
+          >
+            <img
+              src={googlePlay}
+              alt='Available on Google Play'
+              className={classes.img}
+            />
+          </a>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default GetApp;
---
+++ src/components/GetApp/style.ts
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+  box: {
+    padding: 10,
+  },
+  link: {
+    margin: '0 4px',
+  },
+  img: {
+    width: 136,
+  },
+  text: {
+    margin: 10,
+  },
+});
