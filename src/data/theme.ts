1a97ff6a6a905098434b455b533ac8b06a5e9ed8
Shogo Akiyama <sakiyama@knox.edu>
2021-06-28 21:59:13 -0700

    Update theme.ts with new typography and color

--- src/data/theme.ts
+++ src/data/theme.ts
@@ -6,9 +6,30 @@ export const theme = createMuiTheme({
       main: 'rgb(0, 149, 246)',
       light: 'rgba(0, 149, 246, 0.3)',
     },
+    secondary: {
+      main: 'rgb(0, 55, 107)'
+    },
     action: {
       disabledBackground: 'rgba(0, 149, 246, 0.3)',
       disabled: 'rgb(255, 255, 255)',
     },
   },
+  typography: {
+    fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif',
+    body1: {
+      fontWeight: 600, 
+      fontSize: '1.02rem',
+      lineHeight: 1.22,
+    },
+    subtitle1: {
+      fontWeight: 600,
+      fontSize: '0.8rem',
+    },
+    subtitle2: {
+      fontWeight: 400,
+      fontSize: '0.75rem',
+      lineHeight: 1.35,
+      letterSpacing: 'inherit',
+    },
+  }
 });
