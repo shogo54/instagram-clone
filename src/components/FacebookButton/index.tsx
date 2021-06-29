0ffa72443f3e0303e9fbe3f9da526a17e31c9c2e
Shogo Akiyama <sakiyama@knox.edu>
2021-06-28 22:00:18 -0700

    Update FacebookButton to change the color based on the currPage prop

--- src/components/FacebookButton/index.tsx
+++ src/components/FacebookButton/index.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import Button from '../Button';
 
-const FacebookButton: React.FC = () => {
-  return <Button startIcon={<FacebookIcon />}>Log in with Facebook</Button>;
+interface IFacebookButtonProps {
+  currPage: 'signup' | 'login';
+}
+
+const FacebookButton: React.FC<IFacebookButtonProps> = ({ currPage }) => {
+  return (
+    <Button
+      color={currPage === 'login' ? 'secondary' : 'primary'}
+      variant={currPage === 'login' ? 'text': 'contained'}
+      startIcon={<FacebookIcon />}
+    >
+      Log in with Facebook
+    </Button>
+  );
 };
 
 export default FacebookButton;
