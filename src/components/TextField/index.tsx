c016804abb709d635835d9fced96b70019f57c37
Shogo Akiyama <sakiyama@knox.edu>
2021-06-29 00:07:40 -0700

    Update TextField to toggle text display if password

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import MuiTextField, { TextFieldProps } from '@material-ui/core/TextField';
+import { Button, InputAdornment } from '@material-ui/core';
+import MuiTextField, {
+  TextFieldProps as MuiTextFieldProps,
+} from '@material-ui/core/TextField';
 import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
 import { inputStyles, rootStyles } from './style';
+import { useState } from 'react';
+
+type TextFieldProps = MuiTextFieldProps & { signup?: boolean; pass?: boolean };
 
 const TextField: React.FC<TextFieldProps> = ({
   id,
   label,
   value,
   error,
+  signup = false,
+  pass = false,
   ...props
 }) => {
   const rootClasses = rootStyles();
   const inputClasses = inputStyles({ value: value as boolean });
 
-  const adornment = error ? (
+  const [showValue, setShowValue] = useState(!pass);
+
+  let toggleShow = undefined;
+  if (pass && value !== '') {
+    toggleShow = (
+      <Button
+        style={{
+          minWidth: 'unset',
+          textTransform: 'none',
+          backgroundColor: 'inherit',
+          paddingRight: 0,
+        }}
+        disableRipple
+        onClick={() => setShowValue(!showValue)}
+      >
+        {showValue ? 'Hide' : 'Show'}
+      </Button>
+    );
+  }
+
+  const errorIndicator =
+    error && signup ? <CancelOutlinedIcon color='error' /> : null;
+
+  const adornment = (
     <InputAdornment position='end'>
-      <CancelOutlinedIcon color='error' />
+      {errorIndicator}
+      {toggleShow}
     </InputAdornment>
-  ) : null;
+  );
 
   return (
     <MuiTextField
@@ -26,13 +57,17 @@ const TextField: React.FC<TextFieldProps> = ({
       label={label}
       variant='filled'
       placeholder={label?.toString()}
+      type={showValue ? 'text' : 'password'}
       classes={rootClasses}
       InputProps={{
         classes: inputClasses,
         disableUnderline: true,
         endAdornment: adornment,
       }}
-      InputLabelProps={{ shrink: value ? true : false, style: {color: 'rgb(142, 142, 142)'} }}
+      InputLabelProps={{
+        shrink: value ? true : false,
+        style: { color: 'rgb(142, 142, 142)' },
+      }}
       size='small'
       {...props}
     />
